99c1aadb8954ea878fb52568c2f0d7490da7ac24
yungchi <thomas890201@gmail.com>
2021-01-07 13:32:42 +0800

    init

--- Amazing_109_1/settings.py
+++ Amazing_109_1/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,3 +134,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     # another directory ...
 ]
+django_heroku.settings(locals())
